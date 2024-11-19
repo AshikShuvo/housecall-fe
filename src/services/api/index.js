602adad9bb8d24451d8fafb8ee8aaa81f54ee522
AShikShuvo <ashikshuvo1996@gmail.com>
2024-11-19 11:56:02 +0600

    boind pinia store to axios interceptors to handle global loading state on each api call

---
+++ src/services/api/index.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import { useSiteStore } from '@/stores/site.store';
+// import {toast} from "@/hooks/use-toast.ts";
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_BASE_URL
+});
+
+api.interceptors.request.use(
+    (config) => {
+        useSiteStore().setGlobalLoading(true);
+        return config;
+    },
+    (error) => {
+        useSiteStore().setGlobalLoading(false);
+        return Promise.reject(error);
+    }
+);
+
+api.interceptors.response.use(
+    (response) => {
+        useSiteStore().setGlobalLoading(false);
+        return response.data;
+    },
+    (error) => {
+        useSiteStore().setGlobalLoading(false);
+        // toast({
+        //     variant: 'destructive',
+        //     title: 'API Error',
+        //     description: error.response?.data?.message?.message
+        // });
+
+        console.error('API Error:', error.response?.data?.message?.message || error.message);
+        return Promise.reject(error);
+    }
+);
+
+export default api;
